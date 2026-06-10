28a5eff1a012cd29b959bf076cadd2cbfed55afc
yash <yash@123>
2026-06-10 15:48:31 +0530

    feat: add Google Tag Manager tags to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${geist.variable} ${geistMono.variable} ${dmSerif.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KRLD5J4P');`
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className="site-motion font-sans antialiased bg-white text-brand-950" suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KRLD5J4P"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <PageTransition>{children}</PageTransition>
         <WhatsAppFloatingButton />
       </body>
